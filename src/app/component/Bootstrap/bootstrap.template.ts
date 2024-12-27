a130bf38b04d87104556513f78f8fbba0ed6881f
quang1xc <2uang.anh.lee@gmail.com>
2024-12-27 08:23:21 +0700

    first commit

---
+++ src/app/component/Bootstrap/bootstrap.template.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-amount-buttons',
+    standalone: true,
+    imports: [
+    ],
+    templateUrl: './header.template.html',
+    styleUrls: ['./header.template.scss'],
+
+})
+export class HeaderTemplate {
+}
---
+++ src/app/component/Header/header.template.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-amount-buttons',
+    standalone: true,
+    imports: [
+    ],
+    templateUrl: './header.template.html',
+    styleUrl: './header.template.component.scss',
+})
+export class HeaderTemplateComponent {
+}
