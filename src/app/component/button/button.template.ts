a820200205bf2f8f3b029b0e8db845c686a97ced
quang1xc <2uang.anh.lee@gmail.com>
2024-12-27 10:40:09 +0700

    add button ACTIVE STATE

---
+++ src/app/component/button/button.template.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-button',
+    standalone: true,
+    imports: [
+    ],
+    templateUrl: './button.template.html',
+    styleUrls: ['./button.template.scss',
+      '../../assets/css/animate.min.css',
+      '../../assets/css/style.min.css',
+      '../../assets/css/theme/default.css'],
+
+})
+export class ButtonTemplate {
+}
