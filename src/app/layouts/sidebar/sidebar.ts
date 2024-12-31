75dd1f873f5e283a19c30a850b59ff1a194e5af9
quang1xc <2uang.anh.lee@gmail.com>
2024-12-31 15:50:57 +0700

    update(Sidebar): update display  layout when show/hide

--- src/app/layouts/sidebar/sidebar.ts
+++ src/app/layouts/sidebar/sidebar.ts
@@ -1,5 +1,8 @@
-﻿import {Component} from '@angular/core';
+﻿import {Component, Input, OnInit, signal, WritableSignal} from '@angular/core';
 import {RouterModule} from '@angular/router';
+import {NgClass} from '@angular/common';
+import {LayoutService} from '../../services/layout.service';
+import { trigger, style, transition, animate } from '@angular/animations';
 
 
 @Component({
@@ -8,11 +11,26 @@ import {RouterModule} from '@angular/router';
   imports: [RouterModule
   ],
   templateUrl: './sidebar.html',
-  styleUrls: ['./sidebar.scss',
-
-    '../../assets/css/style.min.css',
-    '../../assets/plugins/icon/themify-icons/themify-icons.css'],
+  styleUrls: ['./sidebar.scss'],
+  animations: [
+    trigger('sidebarAnimation', [
+      transition(':enter', [
+        style({ width: '4vw', opacity: 0 }),
+        animate('800ms ease-in-out', style({ width: '15vw', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        animate('800ms ease-in-out', style({ width: '4vw', opacity: 0 }))
+      ])
+    ])
+  ]
 
 })
-export class SidebarTemplate {
+export class SidebarTemplate implements OnInit {
+  @Input() isShowSidebar!: WritableSignal<boolean>;
+  constructor(private layoutService: LayoutService) {
+  }
+  ngOnInit() {}
+  onHideSidebar(){
+    this.layoutService.onHideSidebar();
+  }
 }
---
+++ src/app/services/layout.service.ts
@@ -0,0 +1,13 @@
+﻿import {Injectable} from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LayoutService {
+  private isHideSidebar = new BehaviorSubject<{status: boolean}>({ status: false });
+  statusSidebar = this.isHideSidebar.asObservable();
+  onHideSidebar(){
+    this.isHideSidebar.next({ status: !this.isHideSidebar.value.status });
+  }
+}
