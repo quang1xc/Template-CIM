916da9c7f1c7815c2c9b8455421ecc24c1854cf9
quang1xc <2uang.anh.lee@gmail.com>
2024-12-31 15:45:44 +0700

    feat(StorageService): add service storeage -- get Item -- Remove Item -- Clear

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -12,8 +12,12 @@ export class StorageService {
     }
 
     getItem<T>(key: string): T {
-        const val = localStorage.getItem(key);
-        return JSON.parse(JSON.stringify(key)) as T;
+      const val = localStorage.getItem(key);
+      try {
+        return  JSON.parse(val || '') as T;
+      } catch {
+        return val as T;
+      }
     }
 
     removeItem(key: string) {
